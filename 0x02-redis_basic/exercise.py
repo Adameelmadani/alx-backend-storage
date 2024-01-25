64a2c813123cac9311bd3a7fdd8fa7d08edb05f6
Adam <adamelmadani0129@gmail.com>
2024-01-24 16:19:40 -0800

    1.task

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,8 @@
 """
 This is our module
 """
-import redis, uuid
+import redis
+import uuid
 from typing import Union
 """
 This is redis module
@@ -14,12 +15,14 @@ class Cache:
     """
     This is cache class
     """
+
     def __init__(self):
         """
         This is the constructor function
         """
         self._redis = redis.Redis()
         self._redis.flushdb()
+
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         store function
@@ -27,3 +30,24 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key, fn):
+        """
+        get function
+        """
+        value = self._redis.get(key)
+        if value is None or fn is None:
+            return value
+        return fn(value)
+
+    def get_str(self, key):
+        """
+        get_str function
+        """
+        return self.get(key, str)
+
+    def get_int(self, key):
+        """
+        get_int function
+        """
+        return self.get(key, int)
